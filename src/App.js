cf04a99ae66ca792466943e5f0ecea711f44814a
Daniel Valentine <mrdanvalentine@gmail.com>
2018-02-13 10:07:26 -0600

    createte react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Typing from 'react-typing-animation';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <span className="App-title">Welcome to React</span>
+        </header>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
